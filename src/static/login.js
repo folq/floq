e92844e3e0242cf2aff59f3d9d3eb462b594f1f4
Yu He <yu.he@blankoslo.no>
2016-09-27 17:00:14 +0200

    Fix acceptedEmailDomain for login screen

--- src/static/login.js
+++ src/static/login.js
@@ -1,3 +1,5 @@
+var acceptedEmailDomain = process.env.FLOQ_ACCEPTED_EMAIL_DOMAIN || 'blankoslo.no';
+
 function onSuccess(googleUser) {
     // POST session to backend, which does the redirect.
     var form = document.createElement('form');
@@ -24,7 +26,7 @@ function renderButton() {
     gapi.load('auth2', function() {
         gapi.auth2.init({
             client_id: '1085640931155-0f6l02jv973og8mi4nb124k6qlrh470p.apps.googleusercontent.com',
-            hosted_domain: 'blankoslo.no'
+            hosted_domain: acceptedEmailDomain
         }).then(function() {
             gapi.signin2.render('goog-signin-button', {
                 'width': 250,
