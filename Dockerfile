5ab898da77f2b39088ee568e4d2ed8b7dd55c02a
Yu He <yuhe00@gmail.com>
2017-01-11 18:07:29 +0100

    Fix favicon serving from root

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ FROM mhart/alpine-node
 
 RUN mkdir -p /floq
 WORKDIR /floq
-COPY favicon/* /floq/
+COPY favicon /floq/favicon
 COPY src /floq/src
 COPY package.json /floq/
 COPY start.sh /floq/
--- src/app.js
+++ src/app.js
@@ -62,7 +62,7 @@ app.use(helmet.csp({
 // Redirect all requests to https
 app.use(common.herokuHttpsRedirect);
 app.use('/static', express.static('src/static'));
-app.use('/favicon.ico', express.static('src/static/favicon.ico'));
+app.use('/', express.static('favicon'));
 
 app.use(helmet.noCache())
 
