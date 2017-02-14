999e2664a27552db501d21041e2f615d24238f54
maxmelander <maxelias.melander@gmail.com>
2017-02-14 11:36:18 +0100

    Check if APIToken has expired

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,6 @@
 var Promise = require('promise/lib/es6-extensions');
 var GoogleAuth = require('google-auth-library');
+var common = require('common');
 var ga = new GoogleAuth();
 var jwtClient = new ga.JWTClient();
 
@@ -7,12 +8,25 @@ var aud = '1085640931155-0f6l02jv973og8mi4nb124k6qlrh470p.apps.googleusercontent
 var acceptedEmailDomains =
   (process.env.FLOQ_ACCEPTED_EMAIL_DOMAINS || 'blank.no').split(",");
 
+const TOKEN_BUFFER_SECONDS = 3600 * 12;
+
 function requiresLogin(req, res, next) {
     // TODO: Check if valid employee loaded.
     if (req.session.apiToken) {
+        common.auth.verifyAPIAccessToken(req.session.apiToken)
+            .then(
+                (data) => {
+                    var date = new Date();
+                    var currentTime = date.getTime() / 1000;
+
+                    if (data.exp - currentTime < TOKEN_BUFFER_SECONDS) {
+                        res.redirect('login?to=' + req.originalUrl);
+                    }
+                },
+                (err) => res.status(401).send(err)
+            );
         return next();
     }
-
     res.redirect('/login?to=' + req.originalUrl);
 }
 
